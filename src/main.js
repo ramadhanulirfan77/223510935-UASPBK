1c18c9e8523aae2658f1166028e40459291e3010
ramadhanulirfan77 <irfanramadanul@gmail.com>
2024-06-25 00:42:30 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,17 +2,9 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
-import '@quasar/extras/material-icons/material-icons.css';
-import 'quasar/src/css/index.sass';
-import { Quasar } from 'quasar';
-
 
 
 const app = createApp(App);
-
-app.use(Quasar, {
-  plugins: {} 
-});
 
 app.use(createPinia());
 app.use(router);
